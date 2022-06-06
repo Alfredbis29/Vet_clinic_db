e57423c80bc15bfc960285faa6b52f1c9ba0b3e4
Alfred <kalumunabisimwa5@gmail.com>
2022-06-06 16:42:07 +0200

    add database and table

--- schema.sql
+++ schema.sql
@@ -2,4 +2,4 @@
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR NOT NULL,date_of_birth DATE NOT NULL,escape_attempts INT NOT NULL,neutered 
 BOOL NOT NULL,weight_kg DECIMAL(50,2) NOT NULL
-);
+)
