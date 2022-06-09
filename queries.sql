c9e733a361d7ee6c267f530036df5669600b1402
Alfred <kalumunabisimwa5@gmail.com>
2022-06-09 13:04:04 +0200

    insert data into owners table

--- queries.sql
+++ queries.sql
@@ -1,9 +1,25 @@
-/*Queries that provide answers to the questions from all projects.*/
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered=TRUE AND escape_attempts < 3;
-SELECT date_of_birth from animals WHERE name='Agumon' OR name='Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * from animals WHERE neutered=TRUE;
-SELECT * from animals WHERE name <> 'Gabumon';
-SELECT * from animals WHERE weight_kg >=10.4 and weight_kg <= 17.3;
+/* insert data into the owners */
+INSERT INTO owners (id, full_name, age) 
+VALUES 
+(DEFAULT, 'Sam Smith', 34),
+(DEFAULT, 'Jennifer Orwell', 19),
+(DEFAULT, 'Bob', 45),
+(DEFAULT, 'Melody Pond', 77),
+(DEFAULT, 'Dean Winchester', 14),
+(DEFAULT, 'Jodie Whittaker', 38);
+
+/* insert data into the owners */
+INSERT INTO species (id, name) 
+VALUES 
+(DEFAULT, 'Pokemon'),
+(DEFAULT, 'Digimon');
+
+/* update animals */
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon%';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon%';
+
+UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
